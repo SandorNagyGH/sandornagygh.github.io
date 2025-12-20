06054e9cf14d48206a25df31bfa39e3b6ce9e150
SandorNagyGH <163684000+SandorNagyGH@users.noreply.github.com>
2025-12-20 23:11:08 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,41 @@ function showProjects(){
 }
 
 showBtn.addEventListener("click", showProjects);
+
+// Project Carousel
+const projects = [
+    { img: "https://i.postimg.cc/TYxwmMYx/CRCLImg.jpg", name: "CRCL", link: "https://sandornagygh.github.io/crcl/crcl.html" },
+    { img: "https://i.postimg.cc/SNhD2rXC/Wordle-By-Alex-Img.jpg", name: "Wordle Game", link: "./WordleByAlex/WordleByAlex.html" },
+    { img: "https://i.postimg.cc/nhxgxxY7/uspopulation-Img.jpgg", name: "Choropleth map of Usa", link: "./UsaPopulation/UsaPopulation.html" },
+    { img: "https://i.postimg.cc/yN1NkwB7/Wordle-Solver-Img.jpg", name: "Wordle Solver", link: "./WordleSolver/wordleSolver.html" },
+    { img: "https://i.postimg.cc/28bXh6Tf/Drum-Machine-Img.jpg", name: "Drum Machine", link: "./DrumMachine/DrumMachine.html" },
+    { img: "https://i.postimg.cc/ry0HKXBz/Magyar-Wordle-Image.png", name: "Magyar Wordle", link: "./MagyarWordle/MagyarWordle.html" },
+    { img: "https://cdn.freecodecamp.org/testable-projects-fcc/images/random-quote-machine.png", name: "Random quoteMachine", link: "#" }
+];
+
+let currentIndex = 0;
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const projectImg = document.getElementById('current-project-img');
+const projectName = document.getElementById('project-name');
+const projectLink = document.getElementById('project-link');
+
+function updateCarousel() {
+    projectImg.src = projects[currentIndex].img;
+    projectName.textContent = projects[currentIndex].name;
+    projectLink.href = projects[currentIndex].link;
+}
+
+prevBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + projects.length) % projects.length;
+    updateCarousel();
+});
+
+nextBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % projects.length;
+    updateCarousel();
+});
+
+// Initialize
+updateCarousel();
